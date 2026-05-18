d4c65c2d140fc18a3b949d1f76a55fb59cc8e125
MinhMario <minom35w@gmail.com>
2026-05-18 08:16:40 +0700

    Create btvn3.py

---
+++ btvn3.py
@@ -0,0 +1,5 @@
+name = input("Họ và tên bệnh nhân: ")
+code = input("Mã bệnh án: ")
+department = input("Khoa/Phòng khám chỉ định: ")
+
+print("Bệnh nhân:", name, "- Mã BA:", code, "- Chuyển tới:", department)
